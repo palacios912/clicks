4eff3f6d9163112216414092b2fdeb4fa47c3c2a
lucas palacios <lpalacios168@alumnos.frh.utn.edu.ar>
2023-06-12 17:23:29 -0300

    Signed-off-by: lucas palacios <lpalacios168@alumnos.frh.utn.edu.ar>

---
+++ src/componentes/Contador.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../estilos/Contador.css'
+function Contador({numClicks}){
+    return (
+        <div className='contador'>
+            {numClicks}
+        </div>
+    );
+}
+export default Contador
